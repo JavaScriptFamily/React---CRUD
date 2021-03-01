2686266ab54852484140635d19d2962b9e8b4876
Gurudutt Sharma <gurudutt@thedreamsteps.com>
2021-03-01 15:36:07 +0530

    React Paginate

--- src/Posts/listing.js
+++ src/Posts/listing.js
@@ -1,28 +1,51 @@
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 class Listing extends Component {
     constructor (props) {
         super(props);
         this.state = {
             isLoading: true,
-            items: []
+            offset: 0,
+            data: [],
+            perPage: 10,
+            currentPage: 0
         }
     }
 
     componentDidMount () {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-            .then(res => res.json())
-            .then(json => {
-                this.setState ({
-                    isLoading: false,
-                    items: json
-                });
-            });
+        this.getData();
     }
 
+    getData () {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(data => {
+            const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+
+            this.setState ({
+                isLoading: false,
+                data: slice
+            });
+        });
+    }
+
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+  
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.getData()
+        });
+  
+    };
+
     render () {
-        let { isLoading, items } = this.state;
+        let { isLoading, data } = this.state;
 
         if (isLoading) {
             return (
@@ -47,7 +70,7 @@ class Listing extends Component {
                         </thead>  
                         <tbody>
                             {
-                                items.map(item => (
+                                data.map(item => (
                                     <tr className="Row" key={item.id}>
                                         <td>{item.id}</td>
                                         <td>{item.title}</td>
@@ -61,6 +84,19 @@ class Listing extends Component {
                         </tbody>        
                     </table>
                 </div>
+                
+                <ReactPaginate
+                  previousLabel={"prev"}
+                  nextLabel={"next"}
+                  breakLabel={"..."}
+                  breakClassName={"break-me"}
+                  pageCount={this.state.pageCount}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={5}
+                  onPageChange={this.handlePageClick}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination"}
+                  activeClassName={"active"}/>
             </div>
         );
     }        
